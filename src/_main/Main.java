777c2ece819801be963729a2296b8da54429b3ac
Nathan <contact@meowintons.co.uk>
2023-03-16 21:23:14 +0000

    Cleaned up Main.java Removed old commented code

--- src/_main/Main.java
+++ src/_main/Main.java
@@ -18,28 +18,11 @@ public class Main {
         //Init
         Loader loader = new Loader();
 
-//        TSDatabase database = TSDatabase.Create("Database");
-//        TSObject object = TSObject.Create("Example");
-//        TSField  field  = TSField.Integer("Example Variable", 12);
-//        object.add(field);
-//        database.add(object);
-
         String dir = System.getProperty("user.home") + "/Documents/Bovril Beggars";
         File base = new File(dir);
         if(!base.exists())
             if(base.mkdir()) Logger.log(LogLevel.DEBUG, Main.class.getSimpleName(), "Created Bovril Beggars folder within Documents");
             else Logger.log(LogLevel.WARNING, Main.class.getSimpleName(), "Couldn't create Bovril Beggars folder in Documents");
-
-//        TSWriter writer = new TSWriter();
-//        writer.createFile(dir + "/Example.tsd");
-//        writer.writeDatabaseToFile(database);
-//        writer.close();
-//
-//        TSReader reader = new TSReader();
-//        reader.open(loader, dir + "/Example.tsd");
-//        TSDatabase loaded = TSDatabase.deserialise(reader.getData());
-//        reader.close();
-//        System.out.println(loaded);
 
         ECSManager testManager = new ECSManager();
         testManager.addSystem(new TestSystem());
@@ -57,12 +40,6 @@ public class Main {
         reader.close();
 
         loader.loadECSManagerFromDatabase(testManager, loaded);
-
-        //Update
-//        testManager.update(0);
-
-//        for(Entity e : testManager.getEntities())
-//            Logger.log(LogLevel.DEBUG, Main.class.getSimpleName(), e);
 
         //Clean up
         testManager.destroy();
